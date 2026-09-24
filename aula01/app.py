f8b2129f96d9019f870f9df921b048b09b18afd5
Northon <suporte@univali.br>
2026-09-23 21:44:35 -0300

    Dashboard dos livros com novas funções implementadas e dashboard feita.

--- aula01/app.py
+++ aula01/app.py
@@ -2,6 +2,22 @@
 """
 
 import streamlit as st
+import dados
 
+# Configs da pagina
+st.set_page_config(layout = "wide")
 st.title("📚 Dashboard de Livros")
 st.write("Se você está vendo esta página, o seu ambiente está pronto! 🎉")
+col1, col2, col3 = st.columns(3)
+
+# Calculo dos dados
+livros = dados.ler_livros()
+qntd_livros = len(livros)
+preco_medio = dados.calcular_preco_medio(livros)
+qntd_livros_cinco_estrelas = dados.contar_cinco_estrelas(livros)
+
+# Display dos dados
+col1.metric("Total de livros", qntd_livros)
+col2.metric("Preço médio: ", f"£{preco_medio}")
+col3.metric("Quantidade de livros com cinco estrelas ", qntd_livros_cinco_estrelas)
+st.dataframe(livros)
--- aula01/dados.py
+++ aula01/dados.py
@@ -2,8 +2,77 @@
 """
 
 from pathlib import Path
+import csv
 
 # Pasta onde este arquivo .py está. Assim o programa encontra o CSV
 # mesmo quando é executado a partir de outra pasta (como no Streamlit Cloud).
 PASTA = Path(__file__).parent
 CAMINHO_LIVROS = PASTA / "livros.csv"
+
+def ler_livros():
+    livros = []
+    try:
+        with open(CAMINHO_LIVROS, "r", encoding="utf-8") as arquivo:
+            leitor = csv.DictReader(arquivo)
+            for linha in leitor:
+                livros.append(linha)
+    except FileNotFoundError:
+        print("O arquivo livros.csv não foi encontrado")
+    except Exception as error:
+        print("Ocorreu algum erro na leitura do arquivo: ", error)
+    
+    return livros
+
+def ler_livros_v2():
+    try:
+        with open("livros.csv", "r", encoding="utf-8") as arquivo:
+            print(arquivo.readline())
+    except FileNotFoundError:
+        print("O arquivo livros.csv não foi encontrado")
+    except Exception as error:
+        print("Ocorreu algum erro na leitura do arquivo: ", error)
+
+
+def ler_livros_v1():
+    arquivo = None
+    try:    
+        arquivo = open("livros.csv", "r", encoding="utf-8")
+        print(arquivo.readline())
+    except FileNotFoundError:
+        print("O arquivo livros.csv não foi encontrado")
+    except Exception as error:
+        print("Ocorreu algum erro na leitura do arquivo: ", error)
+    finally:
+        if arquivo is not None:
+            arquivo.close()
+
+def calcular_preco_medio(livros):
+    soma: float = 0
+    for livro in livros:
+        preco_original: str = livro["preco"]
+        preco_original_Limpo: str = preco_original.replace("£", "")
+        preco_num: float = float(preco_original_Limpo)
+        soma += preco_num
+    
+    media: float = soma/len(livros)
+    return round(media, 2)
+
+def contar_cinco_estrelas(livros):
+    cont: int = 0
+    for livro in livros:
+        nota_limpa: str = livro["nota"].lower().strip()
+        if(nota_limpa == "five"):
+            cont = cont + 1
+    
+    return cont
+        
+
+
+
+if __name__ == "__main__":
+    livros = ler_livros()
+    print(f"A quantidade de livros da coleção é de {len(livros)}")
+    preco_medio = calcular_preco_medio(livros)
+    print(f"O preço médio é £{round(preco_medio,2)}")
+    qntd_cinco_estrelas = contar_cinco_estrelas(livros)
+    print(f"Quantidade de livros com cinco estrelas: {qntd_cinco_estrelas}")
